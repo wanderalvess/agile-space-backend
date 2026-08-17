f410a92d2db313a20167e7c1bd32b0e087aa24f4
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2026-08-17 11:09:37 -0300

    chore(setup): initialize Spring Boot backend structure and core dependencies

---
+++ src/main/java/com/agilespace/backend/AgileSpaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agilespace.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileSpaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileSpaceBackendApplication.class, args);
+	}
+
+}
